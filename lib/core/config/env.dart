d6e9960d614d3b5d53c75538fa4409d1be6cddf8
KoshtiHarshal <koshtiharshal51@gmail.com>
2026-05-20 21:31:18 +0530

    chore: remove env.dart from tracking, add to gitignore

--- lib/core/config/env.dart
+++
@@ -1,4 +0,0 @@
-class Env {
-  static const supabaseUrl = 'https://uhofekkdwjtrxpzbofeh.supabase.co';
-  static const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVob2Zla2tkd2p0cnhwemJvZmVoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc4OTU3MzYsImV4cCI6MjA5MzQ3MTczNn0.G-j7x-71-iLbFIYUfazSBgMpohnpDXhfXlw5NZm4WNU';
-}
